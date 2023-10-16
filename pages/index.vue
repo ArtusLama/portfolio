79b14ce0e7c7c3e88bc227f88fdcd19d96370a0e
Artus <artuslama@gmail.com>
2023-10-16 22:25:03 +0200

    remove gh workflow

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="flex flex-col justify-center px-12 md:px-32 child:py-40">
-        <SectionHeader id="home" class="w-full min-h-[100vh]"/>
+        <p class="subTitle">IMPORTANT</p>
+        <p class="text-6xl title">This page is still in progress!</p>
+        <!--<SectionHeader id="home" class="w-full min-h-[100vh]"/>
         <SectionAbout id="about" class="md:w-[50vw]"/>
-        <SectionAbout id="about2" class="md:w-[50vw]"/>
+        <SectionAbout id="about2" class="md:w-[50vw]"/>-->
     </div>
 </template>
 <script lang="ts" setup>
