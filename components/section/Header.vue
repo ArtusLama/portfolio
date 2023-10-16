3c217493f011ff0aaf06ead88a6286b1c9da3058
Artus <artuslama@gmail.com>
2023-10-16 15:10:08 +0200

    add Header

---
+++ components/section/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <SectionTitle
+            sectionName="Welcome, my name is"
+            title="Arthur Paucke"
+            :boldSectionName=true
+        />
+        <p class="mt-2 text-textColor">I’m a Developer and UI/UX Designer</p>
+    </div>
+</template>
