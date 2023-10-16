a51fbbbf29fc3e17589e4f7f9d43152e3acdab12
Artus <artuslama@gmail.com>
2023-10-16 11:46:13 +0200

    Initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>NAVBAR</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+        colors: {
+            primary: "#26A4FF",
+            title: "#CFD8F2",
+            text: "#98A2BD",
+            border: "#3C485B",
+            background: "#0A0E14"
+        },
+        extend: {},
+    }
+}
+
