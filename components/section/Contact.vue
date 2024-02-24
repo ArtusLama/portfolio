64017c175ee9dbe5d7964de07c5918f0c874a99b
Artus <artuslama@gmail.com>
2024-02-24 18:39:16 +0100

    Added Contact Form + DcWebhook

--- components/section/Contact.vue
+++ components/section/Contact.vue
@@ -8,5 +8,66 @@
             So what should I say? I’m a passionate Developer and Designer. Programming is what
             connects me with people and where I learn every day something new.
         </p>
+
+        <form @mouseenter="isInContactSection = true" @mouseleave="isInContactSection = false"
+                 @submit.prevent="sendMessage" class="mt-10 text-white flex flex-col gap-4 justify-between sm:w-[50%] sm:pr-0 pr-10 md:max-w-[20rem]">
+            <div>
+                <label class="block text-textColor font-semibold ml-3" for="fullname">Your Name:</label>
+                <input v-model="nameInput" :required="isInContactSection" type="text" id="fullname" class="w-full mt-1 ml-2 rounded-md bg-backgroundLightColor 
+                    outline-1 outline-none focus:outline-primary
+                    invalid:!outline-red-400 invalid:text-red-100
+                    py-1 pl-2">
+            </div>
+         
+            <div>
+                <label class="block text-textColor font-semibold ml-3" for="email">Your E-Mail:</label>
+                <input v-model="mailInput" :required="isInContactSection" type="email" id="email" class="w-full mt-1 ml-2 rounded-md bg-backgroundLightColor 
+                    outline-1 outline-none focus:outline-primary
+                    invalid:!outline-red-400 invalid:text-red-100
+                    py-1 pl-2">
+            </div>
+
+            <div>
+                <label class="block text-textColor font-semibold ml-3" for="message">Tell me something:</label>
+                <textarea v-model="messageInput" :required="isInContactSection" id="message" class="w-full h-[100px] min-h-[60px] mt-1 ml-2 rounded-md bg-backgroundLightColor
+                    outline-1 outline-none focus:outline-primary
+                    py-1 pl-2"></textarea>
+            </div>
+
+            <button type="submit" class="ml-4 hover:brightness-[0.85] primaryButton transition-all">Send Message</button>
+            <p v-if="messageSent" class="ml-4 -mt-3 text-textColor tracking-tighter text-center">Thank you for your message!</p>
+            
+        </form>
+
     </div>
 </template>
+
+<script setup lang="ts">
+import { useDiscordWebhook } from '~/composables/useDiscordWebhook';
+
+
+    const messageSent = ref(false);
+    const isInContactSection = ref(false);
+
+    const nameInput = ref("");
+    const mailInput = ref("");
+    const messageInput = ref("");
+
+    const sendMessage = () => {
+
+        if (nameInput.value != "" && mailInput.value != "" && messageInput.value != "") {
+            messageSent.value = true;
+            setTimeout(() => messageSent.value = false, 4000);
+
+            
+            useDiscordWebhook().sendContactMessage(nameInput.value, mailInput.value, messageInput.value);
+            
+            
+
+            nameInput.value = "";
+            mailInput.value = "";
+            messageInput.value = "";
+        }
+    }
+
+</script>
---
+++ composables/useDiscordWebhook.ts
@@ -0,0 +1,98 @@
+export const useDiscordWebhook = () => {
+
+    const baseUrl = "https://discord.com/api/webhooks/"
+    const webhook = {
+        contactMessage: "1210967852484399174/NQiTH4vEf3ob3hqowM5u6ivSiWSLjB0OfrjUyj7C9xDqnX2CXienqg-r_DxBv7uFEmdY"
+    }
+    const getIpInfoApiUrl = "http://ip-api.com/json/";
+
+    const getGoogleMapsCoordinateLink = (lat: number, lon: number) => {
+        return "https://www.google.com/maps/search/?api=1&query=" + lat + "," + lon;
+    }
+
+    const getIpInfos = async () => {
+
+
+        const ipInfoRequest = await fetch(getIpInfoApiUrl, { method: "GET", mode: 'cors', headers: { 'Content-Type': 'application/json',}});
+
+        if (ipInfoRequest.ok) {
+            const infos = await ipInfoRequest.json();
+            if (infos.status == "success") {
+                return infos;
+            }
+        }
+        
+        return "error";
+    }
+
+
+    const hexColor = (hex: string) => {
+        return parseInt(hex.replace("#",""), 16);
+    }
+
+
+    const getClientInfoEmbend = async () => {
+        const ipInfos = await getIpInfos();
+        if (ipInfos == "error") {
+            return {
+                title: "Couldn't get Infos!",
+                description: "Cause: " + ipInfos.message,
+                color: hexColor("#f87171"),
+            }
+        }
+        return {
+            color: hexColor("#0fc6b4"),
+            title: "IP: " + ipInfos.query,
+            fields: [
+                {
+                    name: "Country",
+                    value: ipInfos.country,
+                    inline: true,
+                },
+                {
+                    name: "City",
+                    value: ipInfos.city + " | " + ipInfos.zip,
+                    inline: true,
+                },
+                {
+                    name: "Location",
+                    value: "[open GoogleMaps](" + getGoogleMapsCoordinateLink(ipInfos.lat, ipInfos.lon) + ")",
+                    inline: true,
+                },
+                {
+                    name: "Internet Provider",
+                    value: ipInfos.isp,
+                    inline: true,
+                },
+            ]
+        }
+    }
+
+
+    const sendContactMessage = async (fullname: string, mail: string, message: string) => {
+        const request = new XMLHttpRequest();
+        request.open("POST", baseUrl + webhook.contactMessage);
+        request.setRequestHeader('Content-type', 'application/json');
+
+        const data = {
+            content: "Hey <@&1210967295736807476>, **" + fullname + "** sent you a message!",
+            embeds: [
+                {
+                    title: fullname + " sent you this:",
+                    description: message,
+                    timestamp: new Date().toISOString(),
+                    color: hexColor("#26A4FF"),
+                    footer: {
+                        text: mail,
+                    }
+                }, 
+            ],
+        };
+        data.embeds.push(await getClientInfoEmbend());
+
+        request.send(JSON.stringify(data));
+    }
+
+
+    return { sendContactMessage }
+}
