0c3bca48f214a1d290d2b881de824fe9d921a180
Artus <artuslama@gmail.com>
2024-02-24 23:33:06 +0100

    Updating Projects

--- components/section/Projects.vue
+++ components/section/Projects.vue
@@ -9,30 +9,20 @@
             (Also take a look at my <a href="https://github.com/ArtusLama?tab=repositories" target="_blank">repositories</a>)
         </p>
         <div class="flex gap-10 mt-5 flex-wrap">
-            <ProjectsCard title="Test Project"
-                          description="This was a simple tiny project for testing lol. Pretty nice isnt it? Or what do you think ;)"
-                          image-path="images/TestImage.png"
-                          :tags='["Vue", "Typescript", "test123", "Tailwind"]'
-                          github-link="https://github.com"
-                          external-link="https://example.com"/>
-            <ProjectsCard title="Test Project"
-                          description="This was a simple tiny project for testing lol. Pretty nice isnt it? Or what do you think ;)"
-                          image-path="images/TestImage.png"
-                          :tags='["Vue", "Typescript", "test123", "Tailwind"]'
-                          github-link="https://github.com"
-                          external-link="https://example.com"/>
-            <ProjectsCard title="Test Project"
-                          description="This was a simple tiny project for testing lol. Pretty nice isnt it? Or what do you think ;)"
-                          image-path="images/TestImage.png"
-                          :tags='["Vue", "Typescript", "test123", "Tailwind"]'
-                          github-link="https://github.com"
-                          external-link="https://example.com"/>
-            <ProjectsCard title="Test Project"
-                          description="This was a simple tiny project for testing lol. Pretty nice isnt it? Or what do you think ;)"
-                          image-path="images/TestImage.png"
-                          :tags='["Vue", "Typescript", "test123", "Tailwind"]'
-                          github-link="https://github.com"
-                          external-link="https://example.com"/>
+            <ProjectsCard title="Decomposition of a Number"
+                          description="This project was created because of a math challange to make the life easier."
+                          image-path="images/projects/NumberDecomposition.svg"
+                          :tags='["Python"]'
+                          github-link="https://github.com/ArtusLama/decomposition-of-a-number/"/>
+
+
+
+
+            <ProjectsCard title="NOT UP TO DATE"
+                          description="This list is not up to date! You can see my projects on GitHub"
+                          image-path="images/projects/NotAllProjects.svg"
+                          :tags='[]'
+                          github-link="https://github.com/ArtusLama?tab=repositories"/>
         </div>
     </div>
 </template>
